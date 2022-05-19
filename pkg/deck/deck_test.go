cd3d981e1a554369e2bb5302d76c66308e0c71ea
Kaio Brito <kaio.brito92@gmail.com>
2022-05-19 17:59:27 -0300

    Add deck.JSON exporter

--- pkg/deck/deck_test.go
+++ pkg/deck/deck_test.go
@@ -15,8 +15,31 @@ func TestDeal(t *testing.T) {
 		cTypes.NewCard(cTypes.Spades, cTypes.C1),
 	}
 
-	myhand.Print()
 	if !reflect.DeepEqual(expected, myhand) {
 		t.Errorf("My hand doesn't match")
+	}
+}
+
+func TestJson(t *testing.T) {
+	d := Deck{
+		cTypes.NewCard(cTypes.Spades, cTypes.CA),
+		cTypes.NewCard(cTypes.Spades, cTypes.C1),
+	}
+
+	expected := map[string]any{
+		"cards": []map[string]string{
+			{
+				"number": cTypes.CA.Value(),
+				"suit":   cTypes.Spades.Value(),
+			},
+			{
+				"number": cTypes.C1.Value(),
+				"suit":   cTypes.Spades.Value(),
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(expected, d.Json()) {
+		t.Error("JSON doesn't match", expected, d.Json())
 	}
 }
