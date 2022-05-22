c8c360c78b3bf955b0b0d2c87717f96ee1c2bb7d
Kaio Brito <kaio.brito92@gmail.com>
2022-05-22 18:26:47 -0300

    Remove Card custom Marshalling

--- pkg/deck/types/card.go
+++ pkg/deck/types/card.go
@@ -1,27 +1,8 @@
 package deck
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type Card struct {
 	Number CardNumber `json:"number" example:"A"`
 	Suit   CardSuit   `json:"suit" example:"spades"`
-}
-
-func (a *Card) UnmarshalJSON(b []byte) error {
-	var result map[string]string
-
-	if err := json.Unmarshal(b, &result); err != nil {
-		fmt.Println(err)
-		return err
-	}
-	*a = Card{
-		Number: CardNumber(result["number"]),
-		Suit:   CardSuit(result["suit"]),
-	}
-	return nil
 }
 
 func NewCard(suit CardSuit, number CardNumber) Card {
