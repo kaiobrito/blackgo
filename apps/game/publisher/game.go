852916cbba0c7cb39b207f8295d06a8b96b6c3c6
Kaio Brito <kaio.brito92@gmail.com>
2022-05-26 09:14:57 -0300

    Create game publisher

---
+++ apps/game/publisher/game.go
@@ -0,0 +1,21 @@
+package publisher
+
+import "github.com/rabbitmq/amqp091-go"
+
+type Message struct {
+	RoutingKey string
+	Exchange   string
+	Body       []byte
+}
+
+func Publish(ch *amqp091.Channel, message Message) error {
+	return ch.Publish(
+		message.Exchange,   // exchange
+		message.RoutingKey, // routing key
+		false,              // mandatory
+		false,              // immediate
+		amqp091.Publishing{
+			ContentType: "text/plain",
+			Body:        message.Body,
+		})
+}
