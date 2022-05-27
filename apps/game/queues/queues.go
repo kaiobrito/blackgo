8dbfec0297430683fbd8cac827cc05b22dfc9c69
Kaio Brito <kaio.brito92@gmail.com>
2022-05-27 19:53:28 -0300

    Add more game queues

--- apps/game/queues/queues.go
+++ apps/game/queues/queues.go
@@ -2,21 +2,55 @@ package queues
 
 import "blackgo/messaging"
 
+// Exchanges
 var GAMES_ACTION_EXCHANGE messaging.Exchange
+var GAMES_QUERY_EXCHANGE messaging.Exchange
+var GAMES_GET_EXCHANGE messaging.Exchange
 
+// Queues
 var GAMES_CREATE_QUEUE messaging.Queue
+var GAMES_QUERY_QUEUE messaging.Queue
+var GAMES_GET_QUEUE messaging.Queue
 
 func init() {
-	GAMES_ACTION_EXCHANGE = messaging.Exchange{
-		Name:    "games.direct",
-		EType:   "direct",
-		Durable: true,
-	}
+	configExchanges()
 
 	GAMES_CREATE_QUEUE = messaging.Queue{
 		Name:       "games.action",
 		RoutingKey: "create",
 		Exchange:   GAMES_ACTION_EXCHANGE,
 		Durable:    true,
+	}
+
+	GAMES_QUERY_QUEUE = messaging.Queue{
+		Name:       "games.query",
+		RoutingKey: "get_by_id",
+		Exchange:   GAMES_QUERY_EXCHANGE,
+		Durable:    true,
+	}
+
+	GAMES_GET_QUEUE = messaging.Queue{
+		Name:       "games.get",
+		RoutingKey: "get_by_id",
+		Exchange:   GAMES_GET_EXCHANGE,
+		Durable:    false,
+	}
+}
+
+func configExchanges() {
+	GAMES_ACTION_EXCHANGE = messaging.Exchange{
+		Name:    "games.direct",
+		EType:   "direct",
+		Durable: true,
+	}
+	GAMES_QUERY_EXCHANGE = messaging.Exchange{
+		Name:    "games.query",
+		EType:   "direct",
+		Durable: false,
+	}
+	GAMES_GET_EXCHANGE = messaging.Exchange{
+		Name:    "games.get",
+		EType:   "headers",
+		Durable: false,
 	}
 }
